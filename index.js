cf18d86ca5d0c6059236bb65635c94ab25e97ebf
joao-sotero <vitor99jjunior@gmail.com>
2021-03-31 14:44:55 -0300

    criação get, put, post, patch, delete

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
+const { request } = require('express');
 const express = require('express');
+const {v4: uuidv4} = require('uuid');
 const app = express();
 
-console.log(app)
+
+app.use(express.json());
+
+const projects =[];
+
+//GET : BUSCAR INFORMAÇÕES DO BACK-END 
+//POST: CRIAR UMA INFRMAÇÃO NO BACK END
+//PUT/PATCH: ALTERAR UMA INFORMAÇÃO NO BACK-END
+//DELETE: DELETAR INFORMAÇÕES DO BACK-END
+/*
+* Queery params == Vamos usar principalemtne para filtros e paginação
+* Route params ==  identificar recursos na hora de atualizar ou deletar
+* Request body == RESTO DO CONTEUDO NA HORA DE CRIAR OU EDITAR UM RECURSO
+*
+*/
+
+app.get('/projects', (request, response) => {
+   // const{title, owner} = request.query;
+
+    // console.log(title);
+    // console.log(owner)
+
+    return response.json(projects);
+})
+
+app.post('/projects', (request, response)=>{
+    const {title, owner} = request.body;
+
+    const  project = {id: uuidv4(), title, owner};
+
+    projects.push(project); // esse push vai jogar a criação do nosso projeto opara o nosso array
+
+    return response.json(project) // sempre retornar o projeto recem criado e nunca exibit a lista completa
+})
+
+app.put('/projects/:id', (request, response) =>{
+    const params = request.params;
+
+    console.log(params);
+
+    return response.json([
+        'Projeto 50',
+        'projeto 2',
+        'projeto 3',
+        'projeto 4',
+        'projeto 5'
+    ])
+} )
+
+app.delete('/projects/:id', (resquest, response) =>{
+    return response.json([
+        'Projeto 50',
+        'projeto 2',
+    ])
+} )
+
+app.listen(3000, () =>{
+    console.log('servidor rodando!');
+})
